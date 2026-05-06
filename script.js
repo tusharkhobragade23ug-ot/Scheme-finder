48bf21d501c491f6f981f331c61221ad20c9ef18
tusharkhobragade23ug-ot <tushar.khobragade23_ug@apu.edu.in>
2026-05-07 04:22:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+console.log("NEW JS LOADED");
+
+window.findSchemes = function () {
+
+  let incomeInput = document.getElementById("income").value;
+  let income = parseInt(incomeInput) || 0;
+  let caste = document.getElementById("caste").value.toLowerCase();
+  let occupation = document.getElementById("occupation").value.toLowerCase();
+  let disability = document.getElementById("disability").value.toLowerCase();
+
+  //  Prevent empty search
+  if (!incomeInput) {
+    document.getElementById("result").innerHTML =
+      "<div class='scheme-box'>Please enter income</div>";
+    return;
+  }
+
+  fetch("https://tusharkhobragade23ug-ot.github.io/Scheme-finder/schemes.json")
+    .then(response => response.json())
+    .then(data => {
+
+      let results = data.filter(scheme => {
+          console.log(scheme.name, "CHECKING...");
+
+        //  income
+        if (income > scheme.incomeLimit) return false;
+
+        //  caste 
+        if (caste && scheme.caste !== "any" && scheme.caste !== caste)
+          return false;
+
+        //  occupation 
+        if (occupation && scheme.occupation !== "any" && scheme.occupation !== occupation)
+          return false;
+
+        //  disability 
+        if (disability === "yes") {
+          if (scheme.disability !== "yes") return false;
+        }
+        else if (disability === "no") {
+          if (scheme.disability === "yes") return false;
+        }
+
+        return true;
+      });
+
+      console.log("RESULTS:", results);
+
+      let output = "";
+
+      if (results.length === 0) {
+        output = "<div class='scheme-box'>No schemes found</div>";
+      } else {
+        results.forEach(scheme => {
+          output += `
+            <div class="scheme-box">
+              <h4>${scheme.name}</h4>
+              <p>${scheme.description}</p>
+              <a href="${scheme.link}" target="_blank" class="scheme-btn">
+                View Details
+              </a>
+            </div>
+          `;
+        });
+      }
+
+      document.getElementById("result").innerHTML = output;
+    })
+    .catch(error => {
+      console.error("ERROR:", error);
+    });
+};
